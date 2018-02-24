6bd3a9951c1e4d1b4af5c2b9bafdee89ca60b782
Peter <peteral9790@gmail.com>
2018-02-23 20:11:22 -0500

    commit 0.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InspectionDetailsComponent } from './final-inspection/inspection-details/inspection-details.component';
 import { InspectionListComponent } from './final-inspection/inspection-list/inspection-list.component';
 import { InspectionViewComponent } from './final-inspection/inspection-view.component';
@@ -22,6 +23,7 @@ import { InspectionViewComponent } from './final-inspection/inspection-view.comp
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
     ToastrModule.forRoot()
   ],
   providers: [],
--- src/app/final-inspection/inspection-list/inspection-list.component.ts
+++ src/app/final-inspection/inspection-list/inspection-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FinalInspectionService } from '../shared/final-inspection.service';
-import { FinalInspection} from '../shared/final-inspection.model';
+import { FinalInspection } from '../shared/final-inspection.model';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -11,17 +11,19 @@ import { ToastrService } from 'ngx-toastr';
 export class InspectionListComponent implements OnInit {
   showList: boolean = true;
   inspectionHistory: FinalInspection[];
-  constructor(private inspectionService: FinalInspectionService) { }
-  
+  constructor(private inspectionService: FinalInspectionService, private toastr: ToastrService) { }
+
   ngOnInit() {
-    this.inspectionService.getData("FinalInspection", "GetFinalInspectionRecords");
+    this.inspectionService.getData("FinalInspection", "GetInspectionRecords");    
   }
 
   toggleList() {
-    if (this.showList==true) {
+    if (this.showList == true) {
       this.showList = false;
+      this.toastr.success("Got data!", "FInspect");
     } else {
-      this.showList=true;
+      this.showList = true;
+      this.toastr.success("Got data!", "FInspect");
     }
   }
 
