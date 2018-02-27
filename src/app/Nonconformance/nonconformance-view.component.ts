7f8645b4c8267dc8177f4d16f2973aea9cd98978
Peter <peteral9790@gmail.com>
2018-02-26 19:18:48 -0500

    Commit 0.0.6

---
+++ src/app/Nonconformance/nonconformance-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nonconformance-view',
+  templateUrl: './nonconformance-view.component.html',
+  styleUrls: ['./nonconformance-view.component.css']
+})
+export class NonconformanceViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
@@ -10,21 +10,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InspectionDetailsComponent } from './final-inspection/inspection-details/inspection-details.component';
 import { InspectionListComponent } from './final-inspection/inspection-list/inspection-list.component';
 import { InspectionViewComponent } from './final-inspection/inspection-view.component';
+import { NotFoundComponent } from './common/not-found/not-found.component';
+import { NonconformanceViewComponent } from './Nonconformance/nonconformance-view.component';
 
+const appRoutes: Routes = [
+  { path: 'Nonconformance', component: NonconformanceViewComponent },
+  { path: 'FinalInspection', component: InspectionViewComponent },
+  { path: '', redirectTo: '/FinalInspection', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     InspectionDetailsComponent,
     InspectionListComponent,
-    InspectionViewComponent
+    InspectionViewComponent,
+    NotFoundComponent,
+    NonconformanceViewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
